204c019be3683b3b4583ccd26608482a8c601dff
Lars-Petter <lph@hvl.no>
2026-03-10 12:11:48 +0100

    Løsning på eksempler fra f15-timen

---
+++ f15-immutable-sortertliste-losning/src/no/hvl/dat102/immutable_person/PersonTiltak123456.java
@@ -0,0 +1,47 @@
+package no.hvl.dat102.immutable_person;
+
+import java.util.Objects;
+
+public final class PersonTiltak123456 {
+
+	private final NavnMuterbar navn;
+	private final int fodselsaar;
+	
+	public PersonTiltak123456(NavnMuterbar navn, int fodselsaar) {
+		this.navn = new NavnMuterbar(navn.getFornavn(), navn.getEtternavn());
+		this.fodselsaar = fodselsaar;
+	}
+	
+	public NavnMuterbar getNavn() {
+		return new NavnMuterbar(navn.getFornavn(), navn.getEtternavn());
+	}
+
+	public int getFodselsaar() {
+		return fodselsaar;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonTiltak123456{" +
+				"navn=" + navn +
+				", fodselsaar=" + fodselsaar +
+				'}';
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(fodselsaar, navn);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PersonTiltak123456 other = (PersonTiltak123456) obj;
+		return fodselsaar == other.fodselsaar && Objects.equals(navn, other.navn);
+	}
+}
